9b2f991e9d1be8d6584e9f20da8345933e991010
Jason D Thomas <jdthomas24@yahoo.com>
2026-04-04 01:00:42 -0500

    Update Body Driver.groovy

--- Pentair Intellicenter Control/Body Driver.groovy
+++ Pentair Intellicenter Control/Body Driver.groovy
@@ -1,6 +1,6 @@
 // ============================================================
 // Pentair IntelliCenter Body Driver
-// Version: 1.5.0
+// Version: 1.5.1
 // All files in this integration share this version number.
 // ============================================================
 
